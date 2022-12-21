41e9f1004c0a7df4bbbefb86b1a02b94fb84c53c
bm298 <musabilal93@gmail.com>
2022-12-21 02:05:33 +0000

    Updates

---
+++ precache-manifest.bd80cd800b798b2977275c1da857e50e.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = [
+  {
+    "revision": "6fe724f83c63c3260b7e",
+    "url": "/airbnbSite/static/css/main.da03cbef.chunk.css"
+  },
+  {
+    "revision": "6fe724f83c63c3260b7e",
+    "url": "/airbnbSite/static/js/main.6fe724f8.chunk.js"
+  },
+  {
+    "revision": "dc2c6ecaabfff3523052",
+    "url": "/airbnbSite/static/js/1.dc2c6eca.chunk.js"
+  },
+  {
+    "revision": "62da5242e2f131ea0854",
+    "url": "/airbnbSite/static/js/2.62da5242.chunk.js"
+  },
+  {
+    "revision": "65f5d54c312ed2fc251e",
+    "url": "/airbnbSite/static/js/runtime~main.65f5d54c.js"
+  },
+  {
+    "revision": "fe79338a713d12350cfbe19e1b38443d",
+    "url": "/airbnbSite/static/media/airbnblogo.fe79338a.png"
+  },
+  {
+    "revision": "90e811e4e68abd46e6fe2ab2e69167b4",
+    "url": "/airbnbSite/static/media/maldives.90e811e4.jpg"
+  },
+  {
+    "revision": "8e98e259fd3216fe46628b6e35a17e10",
+    "url": "/airbnbSite/static/media/background.8e98e259.jpg"
+  },
+  {
+    "revision": "29d50278b8d5cf6a0b86601af92c0cf7",
+    "url": "/airbnbSite/index.html"
+  }
+];
