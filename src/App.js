9bd1ab3bbd63b8feb5c902a2fa6442c08f485b67
bm298 <musabilal93@gmail.com>
2022-12-22 01:00:49 +0000

    first commit

---
+++ src/App.js
@@ -0,0 +1,35 @@
+import './App.css';
+import './MobResponsive.css';
+import React from 'react';
+import Airbnb2 from './components/airbnb2';
+import NextPage from './nextPageAirbnb/nextPage';
+import {BrowserRouter as Router, Route, Switch, Link} from 'react-router-dom';
+
+function App() {
+
+  const [darkMode, setDarkMode] = React.useState(false)    
+function toggleDarkMode() {
+    setDarkMode(prevMode => !prevMode)
+    darkMode? document.body.style.backgroundColor= "white":document.body.style.backgroundColor= "#222222"
+  }
+
+  return (
+    <div>
+    <Router>
+      <div>
+        <Route exact path="/" 
+        render={(props) => <Airbnb2 {...props} darkMode={darkMode} toggleDarkMode={toggleDarkMode}/> }
+        />
+
+        <Route exact path="/travel"         
+        render={(props) => <NextPage {...props} darkMode={darkMode} toggleDarkMode={toggleDarkMode}/>}
+        />
+      </div>
+    </Router>
+    
+    </div>
+  );
+}
+
+
+export default App;
